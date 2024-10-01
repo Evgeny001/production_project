dd2a8e694b9eeda09db30c564dd2593223177f40
Evgeny <info.elukas@gmail.com>
2024-10-01 14:16:47 +0300

    feat: add condition options.isDev? 'style-loader' :  MiniCssExtractPlugin.loader and add options in buildLoaders function

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,13 @@
 import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
     const cssLoader =       {
         test: /\.s[ac]ss$/i,
         use: [
-            MiniCssExtractPlugin.loader,
+            options.isDev? 'style-loader' :  MiniCssExtractPlugin.loader,
             "css-loader",
             "sass-loader",
         ],
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -17,7 +17,7 @@ return {
     },
     plugins: buildPlugins(options),
     module: {
-        rules: buildLoaders(),
+        rules: buildLoaders(options),
     },
     resolve: buildResolvers(),
     devtool:  isDev ? 'inline-source-map' : undefined,
