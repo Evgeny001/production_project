83213f3240f164fe7b181147ce555ba3d96334d4
Evgeny <info.elukas@gmail.com>
2024-10-04 11:51:57 +0300

    feat: create AboutPage, MainPage and add they in Route, add two Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
+import {Link, Route, Routes} from 'react-router-dom';
 import {Counter} from "./components/Counter";
 import './index.scss'
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
 
 const App = () => {
     return (
         <div className={'app'}>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>о сайте</Link>
         <Routes>
-         <Route path={'/about'}/>
-         <Route path={'/'}/>
+         <Route path={'/about'} element={<AboutPage/>}/>
+         <Route path={'/'} element={<MainPage/>}/>
         </Routes>
         </div>
     );
