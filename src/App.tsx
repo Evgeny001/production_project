22da259dd9a4f1bb13c302cffa93b01165612d61
Evgeny <info.elukas@gmail.com>
2024-10-08 19:35:02 +0300

    feat: add toggleTheme to switch the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss'
 import './index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
+
 const App = () => {
+    const [theme, setTheme] = useState(Theme.LIGHT)
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+    }
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>о сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
