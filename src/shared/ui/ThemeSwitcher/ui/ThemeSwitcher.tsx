174ab29203f422d712f6e710d6be78b98f15b12e
Evgeny <info.elukas@gmail.com>
2024-10-20 15:49:10 +0300

    feat: add Theme to index.ts to import as public API, add TS type for svg, add ternary operator to switch <DarkIcon/><LightIcon/>

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import {useTheme} from "app/providers/ThemeProvider";
+import {Theme, useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import cls  from "./ThemeSwitcher.module.scss"
+import cls from "./ThemeSwitcher.module.scss"
+import LightIcon from 'shared/assets/icons/theme-light.svg'
+import DarkIcon from 'shared/assets/icons/theme-dark.svg'
+
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -9,7 +12,9 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher = ({className}:ThemeSwitcherProps) => {
     const {theme, toggleTheme} = useTheme()
     return (
-        <button className={classNames(cls.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>TOGGLE</button>
+        <button className={classNames(cls.ThemeSwitcher, {}, [className])} onClick={toggleTheme}>
+            {theme === Theme.DARK? <DarkIcon/> : <LightIcon />}
+        </button>
     );
 };
 
