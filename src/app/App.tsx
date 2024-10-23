f84eaca7e37f4eeb632b367d0ad2aae2547a94f7
Evgeny <info.elukas@gmail.com>
2024-10-23 23:24:03 +0300

    feat: add keys translation.json and Component with language toggle function, import "./shared/config/i18n/i18n";

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './styles/index.scss';
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
+import i18n from "i18next";
 
-
+const Component = () => {
+    const {t} = useTranslation()
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    }
+    return <div>
+        <button onClick={toggle}>{t('Перевод')}</button>
+        {t('Тестовый пример')}
+    </div>
+}
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar/>
-            <div className="content-page">
-                <Sidebar/>
-                <AppRouter/>
-            </div>
+            <Suspense fallback=''>
+                <Navbar/>
+                <Component/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     );
 };
