5524b3b2abdae067b88e1f40ec6c00b2147ae931
Evgeny <info.elukas@gmail.com>
2024-10-24 11:58:02 +0300

    fix: translate in locales/ru/en/about.json, /main.json add LangSwitcher and call it in Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,26 +5,13 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
-import {useTranslation} from "react-i18next";
-import i18n from "i18next";
 
-const Component = () => {
-    const {t} = useTranslation()
-    const toggle = () => {
-        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-    }
-    return <div>
-        <button onClick={toggle}>{t('Перевод')}</button>
-        {t('Тестовый пример')}
-    </div>
-}
 const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback=''>
                 <Navbar/>
-                <Component/>
                 <div className="content-page">
                     <Sidebar/>
                     <AppRouter/>
