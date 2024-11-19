5ec8f651e062b1fef637ec239e8ebd7e9a32d986
Evgeny <info.elukas@gmail.com>
2024-11-19 12:39:09 +0300

    feat: add 3 test in classNames.test.ts

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -2,6 +2,27 @@ import { classNames } from './classNames';
 
 describe('classNames', () => {
     test('test', () => {
+        expect(true).toBe(true);
+    });
+});
+
+describe('classNames', () => {
+    test('test', () => {
         expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with mods true', () => {
+        const expected = 'someClass class1 class2 hovered scrollable';
+        expect(classNames('someClass', { hovered: true, scrollable: true }, ['class1', 'class2'])).toBe(expected);
+    });
+
+    test('with additional false', () => {
+        const expected = 'someClass class1 class2 hovered';
+        expect(classNames('someClass', { hovered: true, scrollable: false }, ['class1', 'class2'])).toBe(expected);
+    });
+
+    test('with additional undefined', () => {
+        const expected = 'someClass class1 class2 hovered';
+        expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['class1', 'class2'])).toBe(expected);
     });
 });
