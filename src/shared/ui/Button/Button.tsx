e9aca0c740b74d6b14ce3d406e0a9c9193820537
Evgeny <info.elukas@gmail.com>
2024-12-04 12:44:08 +0300

    feat: integrate Storybook - Moved Storybook to config/storybook and updated paths. - Configured Webpack for resolving modules and extensions. - Added `buildCssLoader` for SCSS/CSS handling in dev/prod modes. refactor: extract `buildCssLoader` to a separate module config/build/loaders/buildCssLoader.ts feat: add Storybook stories for Button component

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,7 +1,7 @@
 import { ButtonHTMLAttributes, FC } from 'react';
 
+import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
-import { classNames } from '../../lib/classNames/classNames';
 
 export enum ThemeButton {
     CLEAR = 'clear',
