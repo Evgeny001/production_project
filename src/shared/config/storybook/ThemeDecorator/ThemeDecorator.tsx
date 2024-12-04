937f4712883be417dd825ed3626a3a389dc75662
Evgeny <info.elukas@gmail.com>
2024-12-04 22:59:06 +0300

    feat: add Sidebar story to Storybook, update storybook webpack config to handle SVGs

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,6 +1,7 @@
 import { Theme } from 'app/providers/ThemeProvider';
 import { Story } from '@storybook/react';
 
+// eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
     <div className={`app ${theme}`}>
         <StoryComponent />
--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {  ComponentMeta, ComponentStory } from '@storybook/react';
-import { Button, ThemeButton } from './Button';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
+import { Button, ThemeButton } from './Button';
 
 export default {
     title: 'shared/Button',
