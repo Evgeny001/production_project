405e2afb37872e1416956b1b219efad8a96e9e9f
Evgeny <info.elukas@gmail.com>
2024-12-10 00:04:46 +0300

    feat: add draft modal component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,14 +5,21 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import {Modal} from "shared/ui/Modal/Modal";
 
 function App() {
     const { theme } = useTheme();
-
+    const [isOpen, setIsOpen] = React.useState(false);
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={()=> setIsOpen(true)}>
+                    toggle
+                </button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    dsfjdoghjbjfcklxnvdklxcngjkbfdlcvnbklcxfgndfkjngjkdfnxgfd
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
