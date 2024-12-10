92b2f960c1cb335c2c563e0e6d884c9969549b98
Evgeny <info.elukas@gmail.com>
2024-12-10 10:57:41 +0300

    feat: add smooth modal closing and Escape key handling, add useEffect to add/remove event listeners

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Modal.module.scss'
-import React, {ReactNode} from "react";
+import React, {ReactNode, useCallback, useEffect, useRef, useState} from "react";
 
 interface ModalProps {
     className?: string;
@@ -8,22 +8,53 @@ interface ModalProps {
     isOpen?: boolean;
     onClose?: () => void;
 }
+
+const ANIMATION_DELAY = 300;
+
 export const Modal = (props: ModalProps) => {
     const { className,
         children,
         isOpen,
         onClose
     } = props;
+
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>()
+
     const closeHandler = () => {
         if(onClose) {
-            onClose()
+            setIsClosing(true)
+            timerRef.current = setTimeout(() => {
+                console.log('hi')
+                onClose()
+                setIsClosing(false)
+            }, ANIMATION_DELAY)
         }
     }
+
+    const onKeydown = useCallback((e: KeyboardEvent) => {
+        if(e.key === 'Escape') {
+            closeHandler()
+        }
+    }, [closeHandler])
+
     const onContentClick = (e: React.MouseEvent) => {
     e.stopPropagation();
     }
+
+    useEffect(() => {
+        if(isOpen) {
+            window.addEventListener('keydown', onKeydown);
+        }
+        return () => {
+            clearTimeout(timerRef.current);
+            window.removeEventListener('keydown', onKeydown);
+        }
+    }, [isOpen, onKeydown]);
+
     const mods: Record<string, boolean> = {
-    [cls.opened]: isOpen
+    [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing
     }
 
 return (
