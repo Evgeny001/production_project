0580f7588e4523df434f83fabbc70f2634f8eb63
Evgeny <info.elukas@gmail.com>
2024-12-11 23:41:12 +0300

    feat: implement complete Counter module using RTK

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BugButton } from 'app/providers/ErrorBoundary';
+import { Counter } from 'entities/Counter';
 
 function MainPage() {
     const { t } = useTranslation('main');
@@ -8,6 +9,7 @@ function MainPage() {
         <div>
             <BugButton />
             {t('Главная страница')}
+            <Counter />
         </div>
     );
 }
