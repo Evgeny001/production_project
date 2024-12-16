87e5eecf588001329db2d937feab97d2bbc4afff
Evgeny <info.elukas@gmail.com>
2024-12-16 15:27:16 +0300

    feat: add custom Input refactor: fix lint css and change colors in dark & normal themes

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 import cls from './LoginForm.module.scss';
 
 interface LoginFormProps {
@@ -11,8 +12,17 @@ export const LoginForm = ({ className }: LoginFormProps) => {
     const { t } = useTranslation();
     return (
         <div className={classNames(cls.LoginForm, {}, [])}>
-            <input type="text" className={cls.input} />
-            <input type="text" className={cls.input} />
+            <Input
+                autoFocus
+                type="text"
+                className={cls.input}
+                placeholder={t('Введите username')}
+            />
+            <Input
+                type="text"
+                className={cls.input}
+                placeholder={t('Введите пароль')}
+            />
             <Button className={cls.loginBtn}>
                 {t('Войти')}
             </Button>
