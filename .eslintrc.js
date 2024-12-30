7a572b8b58039362369c89b03473e371057239f7
Evgeny <info.elukas@gmail.com>
2024-12-30 23:53:25 +0300

    feat: add Profile module - add Currency and Country enums for profile data - add interfaces for profile entities - add profileSlice for state management - updated StateSchema with optional profile state - used DynamicModuleLoader for lazy-loaded reducers in ProfilePageexports with named exports for consistency and style.

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,10 +30,11 @@ module.exports = {
         'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
-        'no-unused-vars': ['warn', {
-            varsIgnorePattern: '^_', // Игнорирует переменные, начинающиеся с '_'
-            argsIgnorePattern: '^_', // Игнорирует параметры функций, начинающиеся с '_'
-        }],
+        // 'no-unused-vars': ['warn', {
+        //     varsIgnorePattern: '^_|^(Currency|Country)$',
+        //     argsIgnorePattern: '^_', // Игнорирует параметры функций, начинающиеся с '_'
+        // }],
+        'no-unused-vars': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'off',
