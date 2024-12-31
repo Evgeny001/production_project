b09a4280dd7727a3a3285627a6ee9891d26f0a0c
Evgeny <info.elukas@gmail.com>
2024-12-31 14:34:00 +0300

    feat: add navigate as extra argument in configureStore

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -6,6 +6,8 @@ import {
 } from '@reduxjs/toolkit';
 import { ProfileSchema } from 'entities/Profile';
 import { AxiosInstance } from 'axios';
+import { To } from 'history';
+import { NavigateOptions } from 'react-router';
 
 export interface StateSchema {
     counter: CounterSchema
@@ -30,6 +32,7 @@ export interface ReduxStoreWithManager extends EnhancedStore<StateSchema>{
 
 export interface ThunkExtraArg {
     api: AxiosInstance;
+    navigate: (to: To, options?: NavigateOptions) => void
 }
 
 export interface ThunkConfig<T> {
--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -3,11 +3,14 @@ import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { createReducerManager } from 'app/providers/StoreProvider/config/reducerManager';
 import { $api } from 'shared/api/api';
+import { To } from 'history';
+import { NavigateOptions } from 'react-router';
 import { StateSchema } from './StateSchema';
 
 export function createReduxStore(
     initialState?: StateSchema,
     asyncReducers?: ReducersMapObject<StateSchema>,
+    navigate? : (to: To, options?: NavigateOptions) => void,
 ) {
     const rootReducer: ReducersMapObject<StateSchema> = {
         ...asyncReducers,
@@ -25,6 +28,7 @@ export function createReduxStore(
             thunk: {
                 extraArgument: {
                     api: $api,
+                    navigate,
                 },
             },
         }),
