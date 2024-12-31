61ecd59698ae271d46258cfdd1db7bbc95962810
Evgeny <info.elukas@gmail.com>
2024-12-31 15:10:09 +0300

    feat: add global variable __API__ for backend URL configuration based on build environment

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { USER_LOCAL_STORAGE_KEY } from 'shared/const/localStorage';
 
+// const baseUrl = __IS_DEV__ ? 'http://localhost:8000' : '';
+
 export const $api = axios.create({
-    baseURL: 'http://localhost:8000',
+    // baseURL: baseUrl,
+    baseURL: __API__,
     headers: {
         authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY),
     },
