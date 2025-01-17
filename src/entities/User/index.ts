ee3dc64eb91c45342ce3014720936022869acf6d
Evgeny <info.elukas@gmail.com>
2025-01-17 12:34:52 +0300

    feat: delay AppRouter rendering until user initialization is complete

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
 export { userActions, userReducer } from './model/slice/userSlice';
 export { User, UserSchema } from './model/types/user';
 export { getUserAuthData } from './model/selector/getUserAuthData/getUserAuthData';
+export { getUserInited } from './model/selector/getUserInited/getUserInited';
