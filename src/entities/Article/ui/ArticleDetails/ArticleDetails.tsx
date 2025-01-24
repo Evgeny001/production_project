f922cda9a71144b487e2583c80d5ce08204d87a0
Evgeny <info.elukas@gmail.com>
2025-01-24 13:26:46 +0300

    fix: add keys to renderBlock in ArticleDetails feat: add stories for ArticleDetails and ArticleDetailsPage

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -55,18 +55,20 @@ export const ArticleDetails = memo((props : ArticleDetailsProps) => {
     const renderBlock = useCallback((block: ArticleBlock) => {
         switch (block.type) {
         case ArticleBlockType.CODE:
-            return <ArticleCodeBlockComponent block={block} className={cls.block} />;
+            return <ArticleCodeBlockComponent key={block.id} block={block} className={cls.block} />;
         case ArticleBlockType.IMAGE:
-            return <ArticleImageBlockComponent block={block} className={cls.block} />;
+            return <ArticleImageBlockComponent key={block.id} block={block} className={cls.block} />;
         case ArticleBlockType.TEXT:
-            return <ArticleTextBlockComponent className={cls.block} block={block} />;
+            return <ArticleTextBlockComponent key={block.id} className={cls.block} block={block} />;
         default:
             return null;
         }
     }, []);
 
     useEffect(() => {
-        dispatch(fetchArticleById(id));
+        if (__PROJECT__ !== 'storybook') {
+            dispatch(fetchArticleById(id));
+        }
     }, [dispatch, id]);
 
     let content;
