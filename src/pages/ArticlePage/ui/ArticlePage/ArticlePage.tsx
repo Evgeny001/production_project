c6803ead21867713afe7b3605560fa71c9ce289c
Evgeny <info.elukas@gmail.com>
2025-02-26 17:20:59 +0300

    feat: add tiled and list svg, ArticleViewSelector for changeView

--- src/pages/ArticlePage/ui/ArticlePage/ArticlePage.tsx
+++ src/pages/ArticlePage/ui/ArticlePage/ArticlePage.tsx
@@ -1,18 +1,23 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
-import { ArticleList } from 'entities/Article';
+import { memo, useCallback } from 'react';
+import { ArticleList, ArticleView } from 'entities/Article';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useSelector } from 'react-redux';
+import { ArticleViewSelector } from 'features/ArticleViewSelector/ArticleViewSelector';
 import { fetchArticlesList } from '../../model/services/fetchArticlesList/fetchArticlesList';
 import {
     getArticlesPageError,
     getArticlesPageIsLoading,
     getArticlesPageView,
 } from '../../model/selector/articlesPageSelectors';
-import { articlesPageReducer, getArticles } from '../../model/slices/ArticlesPageSlice/articlesPageSlice';
+import {
+    articlesPageActions,
+    articlesPageReducer,
+    getArticles,
+} from '../../model/slices/ArticlesPageSlice/articlesPageSlice';
 
 interface ArticlePageProps {
     className?: string;
@@ -30,6 +35,11 @@ const ArticlePage = (props: ArticlePageProps) => {
     const isLoading = useSelector(getArticlesPageIsLoading);
     const error = useSelector(getArticlesPageError);
     const view = useSelector(getArticlesPageView);
+
+    const onChangeView = useCallback((view: ArticleView) => {
+        dispatch(articlesPageActions.setView(view));
+    }, [dispatch]);
+
     useInitialEffect(() => {
         dispatch(fetchArticlesList());
     });
@@ -37,6 +47,7 @@ const ArticlePage = (props: ArticlePageProps) => {
     return (
         <DynamicModuleLoader reducers={reducers}>
             <div className={classNames('cls.ArticlePage', {}, [className])}>
+                <ArticleViewSelector view={view} onViewClick={onChangeView} />
                 <ArticleList
                     isLoading={isLoading}
                     view={view}
