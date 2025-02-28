a4df1f06acae47e5f1c2734e552dca6b3f286583
Evgeny <info.elukas@gmail.com>
2025-02-28 17:05:09 +0300

    feat: add useInfiniteScroll and use it in Page

---
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -0,0 +1,33 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+export interface UseInfiniteScrollProps {
+callback: () => void;
+triggerRef: MutableRefObject<HTMLElement>
+wrapperRef: MutableRefObject<HTMLElement>
+}
+
+export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfiniteScrollProps) {
+    useEffect(() => {
+        const options = {
+            root: wrapperRef.current,
+            rootMargin: '0px',
+            threshold: 1.0,
+        };
+
+        const observer = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting) {
+                callback();
+                console.log('intersected');
+            }
+        }, options);
+
+        observer.observe(triggerRef.current);
+
+        return () => {
+            if (observer) {
+                // eslint-disable-next-line react-hooks/exhaustive-deps
+                observer.unobserve(triggerRef.current);
+            }
+        };
+    }, [triggerRef, wrapperRef]);
+}
--- src/shared/ui/Page/Page.tsx
+++ src/shared/ui/Page/Page.tsx
@@ -1,5 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { memo, ReactNode } from 'react';
+import {
+    memo, MutableRefObject, ReactNode, useRef,
+} from 'react';
+import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
 import cls from './Page.module.scss';
 
 interface PageProps {
@@ -9,9 +12,20 @@ interface PageProps {
 
 export const Page = memo((props: PageProps) => {
     const { className, children } = props;
+
+    const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+    useInfiniteScroll({
+        wrapperRef,
+        triggerRef,
+        callback: () => console.log('callback'),
+    });
+
     return (
-        <section className={classNames(cls.Page, {}, [className])}>
+        <section ref={wrapperRef} className={classNames(cls.Page, {}, [className])}>
             {children}
+            <div ref={triggerRef} />
         </section>
     );
 });
