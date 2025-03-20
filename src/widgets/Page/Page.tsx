9e2a5624c6f3316aa8b1485b2452ec0492b30a04
Evgeny <info.elukas@gmail.com>
2025-03-20 14:10:15 +0300

    feat: add useThrottle

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -1,6 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import {
-    memo, MutableRefObject, ReactNode, useRef, UIEvent, useEffect,
+    memo, MutableRefObject, ReactNode, useRef, UIEvent,
 } from 'react';
 import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
@@ -9,6 +9,7 @@ import { useLocation } from 'react-router-dom';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { useSelector } from 'react-redux';
 import { StateSchema } from 'app/providers/StoreProvider';
+import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
 import cls from './Page.module.scss';
 
 interface PageProps {
@@ -36,12 +37,12 @@ export const Page = memo((props: PageProps) => {
         wrapperRef.current.scrollTop = scrollPosition;
     });
 
-    const onScroll = (e: UIEvent<HTMLDivElement>) => {
+    const onScroll = useThrottle((e: UIEvent<HTMLDivElement>) => {
         dispatch(scrollSaveActions.setScrollPosition({
             path: pathname,
             position: e.currentTarget.scrollTop,
         }));
-    };
+    }, 500);
 
     return (
         <section ref={wrapperRef} className={classNames(cls.Page, {}, [className])} onScroll={onScroll}>
